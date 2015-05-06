3ce4efbcc6d0bb511a4420a83f931cf2b6a0deb7
Yash Shah <yshah@mail.uoguelph.ca>
2015-05-06 15:15:10 -0400

    eee

--- php/login.php
+++ php/login.php
@@ -1,79 +1,83 @@
 <?php
+
 session_start(); // Starting Session
 $errorlogin = ''; // Variable To Store Error Message
 $errorregister = ''; //Variable to Store Error Message
 if (isset($_POST['submitlogin'])) {
-// Define $username and $password
+    // Define $username and $password
     $username = $_POST['username'];
     $password = $_POST['password'];
-// Establishing Connection with Server by passing server_name, user_id and password as a parameter
+    $host = "host=ec2-23-23-81-221.compute-1.amazonaws.com";
+    $port = "port=5432";
+    $dbname = "dbname=dfqf1tisgv020h";
+    $credentials = "user=aeqorodridmopp password=UdZfcpfn1ViPdEnvoYmug0BAIw";
 
-    $connection = mysqli_connect("ec2-23-23-81-221.compute-1.amazonaws.com", "aeqorodridmopp", "UdZfcpfn1ViPdEnvoYmug0BAIw", "dfqf1tisgv020h");
-// Check connection
-    if (mysqli_connect_errno()) {
-        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    $db = pg_connect("$host $port $dbname $credentials");
+    if (!$db) {
+        echo "Error : Unable to open database\n yash";
+    } else {
+        echo "Opened database successfully yash \n";
     }
-//$connection = mysql_connect("localhost", "root", "yashshah555");Deprecated
-// To protect MySQL injection for Security purpose
-    $username = stripslashes($username);
-    $password = stripslashes($password);
-    $username = mysqli_real_escape_string($connection, $username);
-    $password = mysqli_real_escape_string($connection, $password);
-// Selecting Database
-//$db = mysql_select_db("login", $connection);Deprecated
-//if(!$db) {
-//     echo("no database!\n");
-//    exit();
-//}
-// SQL query to fetch information of registerd users and finds user match.
-    $query = mysqli_query($connection, "select * from users where password='$password' AND username='$username'");
-    $rows = mysqli_num_rows($query);
+    $sql = <<<EOF
+      SELECT * from oneword.users where UserName = '$username' AND Password = '$password';
+EOF;
+
+    $ret = pg_query($db, $sql);
+    if (!$ret) {
+        echo pg_last_error($db);
+        exit;
+    }
+    $rows = pg_num_rows($query);
     if ($rows == 1) {
-        $_SESSION['login_user'] = $username; // Initializing Session
-        header("location: profile.php"); // Redirecting To Other Page
+        echo "Login successfully $username\n";
+//        $_SESSION['login_user'] = $username; // Initializing Session
+//        header("location: profile.php"); // Redirecting To Other Page
     } else {
         $errorlogin = "Username or Password is invalid";
     }
-    mysqli_close($connection); // Closing Connection
+    //mysqli_close($connection); // Closing Connection
+    pg_close($connection);
 }
+/*
+  //for register button - add a new entry to database
+  else if (isset($_POST['submitregister'])) {
+  if ($_POST['password'] != $_POST['passwordAgain']) {
+  $errorregister = "Passwords does not match";
+  ?><script>
+  $('#login').hide();
+  $('#register').show();
+  </script>
+  <?php
+  } else {
+  // Define $username and $password
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+  // Establishing Connection with Server by passing server_name, user_id and password as a parameter
+  $connection = mysqli_connect("ec2-23-23-81-221.compute-1.amazonaws.com", "aeqorodridmopp", "UdZfcpfn1ViPdEnvoYmug0BAIw", "dfqf1tisgv020h");
+  // Check connection
+  if (!$connection) {
+  die("Connection failed: " . mysqli_connect_error());
+  //  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+  //// To protect MySQL injection for Security purpose
+  //$username = stripslashes($username);
+  //$password = stripslashes($password);
+  //$username = mysqli_real_escape_string($connection,$username);
+  //$password = mysqli_real_escape_string($connection,$password);
+  //Insert Query
+  $sql = "INSERT INTO users (UserName, Password)
+  VALUES ('$username', '$password')";
+  if (mysqli_query($connection, $sql)) {
+  echo "New record created successfully";
 
-//for register button - add a new entry to database
-else if (isset($_POST['submitregister'])) {
-    if ($_POST['password'] != $_POST['passwordAgain']) {
-        $errorregister = "Passwords does not match";
-        ?><script>
-            $('#login').hide();
-            $('#register').show();
-        </script>
-        <?php
-    } else {
-// Define $username and $password
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-// Establishing Connection with Server by passing server_name, user_id and password as a parameter
-        $connection = mysqli_connect("ec2-23-23-81-221.compute-1.amazonaws.com", "aeqorodridmopp", "UdZfcpfn1ViPdEnvoYmug0BAIw", "dfqf1tisgv020h");
-// Check connection
-        if (!$connection) {
-            die("Connection failed: " . mysqli_connect_error());
-//  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-        }
-//// To protect MySQL injection for Security purpose
-//$username = stripslashes($username);
-//$password = stripslashes($password);
-//$username = mysqli_real_escape_string($connection,$username);
-//$password = mysqli_real_escape_string($connection,$password);
-        //Insert Query
-        $sql = "INSERT INTO users (UserName, Password)
-VALUES ('$username', '$password')";
-        if (mysqli_query($connection, $sql)) {
-            echo "New record created successfully";
+  $_SESSION['login_user'] = $username; // Initializing Session
+  header("location: profile.php"); // Redirecting To Other Page
+  } else {
+  echo "Error: " . $sql . "<br>" . $connection->error;
+  }
+  mysqli_close($connection); // Closing Connection}
+  }
+  }
 
-            $_SESSION['login_user'] = $username; // Initializing Session
-            header("location: profile.php"); // Redirecting To Other Page
-        } else {
-            echo "Error: " . $sql . "<br>" . $connection->error;
-        }
-        mysqli_close($connection); // Closing Connection}
-    }
-}
+ */
 ?>
